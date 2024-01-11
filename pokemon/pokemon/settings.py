9810e26b5ed247dc0936338e4e112dafe37dd7fc
Calvin Pun <cpun40@stuy.edu>
2024-01-11 14:49:03 -0500

    started html

--- pokemon/pokemon/settings.py
+++ pokemon/pokemon/settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'pokedex'
 ]
 
 MIDDLEWARE = [
--- pokemon/pokemon/urls.py
+++ pokemon/pokemon/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pokedex/', include('pokedex.urls', namespace='pokedex'))
+    path('', include('pokedex.urls', namespace='pokedex'))
 ]
