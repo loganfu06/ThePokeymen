dae3f08be6fbadc3827de24c33c5af78420cae56
Logan Fu <lfu40@stuy.edu>
2024-01-11 14:45:52 -0500

    worked on creating a view for inserting data into database

--- pokemon/pokedex/urls.py
+++ pokemon/pokedex/urls.py
@@ -6,4 +6,5 @@ app_name = "pokedex"
 
 urlpatterns = [
     path("", views.PokemonListView.as_view(), name="pokemon_list"),
+    path("create/<str:pokemon_name>", views.createPokemon, name="pokemon_create"),
 ]
--- pokemon/pokedex/views.py
+++ pokemon/pokedex/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.contrib import messages
+from django.http import HttpResponse
 
 from .models import Pokemon, Type
 
 # Create your views here.
 class PokemonListView(ListView):
     model = Pokemon
+    
+def createPokemon(request, pokemon_name):
+    print("this works")
+    return redirect("pokedex:pokemon_list")
