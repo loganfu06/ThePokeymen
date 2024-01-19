46b4a36bbff3c2300158876e35020f172f4a6358
3JJ003 <jyap40@stuy.edu>
2024-01-19 12:20:28 -0500

    disable and reinable the list

--- pokemon/core/static/script.js
+++ pokemon/core/static/script.js
@@ -1,20 +1,36 @@
-//document.getElementById("myDropdown").classList.toggle("show");
-const SEARCH = document.getElementById("search");
 const SHOWPOKEMON = document.getElementById("showPokemon");
-const POKELIST = document.getElementById("pokelist");
+const POKELIST = document.getElementsByClassName(".pokelist");
+const MYINPUT = document.getElementById("myInput");
 
+SHOWPOKEMON.classList.add("hidden");
 function filter() {
+    console.log("OKOK");
+    //const SEARCH = document.getElementById(".search");
 
-    SHOWPOKEMON.removeAttribute("hidden");
+    //document.getElementById("showPokemon").classList.toggle("hidden");
+
+    // if (SHOWPOKEMON.classList.contains("hidden")) {
+    //     SHOWPOKEMON.classList.remove("hidden");
+    // }
     
     for (i = 0; i < POKELIST.length; i++) {
-        txtValue = a[i].textContent || a[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
+        for (let j = 0; j < MYINPUT.length; j++) {
+            if (POKELIST.indexOf(i).slice(0,j) == MYINPUT.value.slice(0,j)) {
+                POKELIST.indexOf(i).classList.remove("hidden");
+            } else {
+                POKELIST.indexOf(i).style.display = "none";
+            }
         }
     }
+
+    // for (i = 0; i < POKELIST.length; i++) {
+    //     txtValue = a[i].textContent || a[i].innerText;
+    //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    //         a[i].classList.remove("hidden");
+    //     } else {
+    //         a[i].style.display = "none";
+    //     }
+    // }
 
     //var pokelist = document.getElementById("pokelist").length;
     // for (var i = 0; i < pokelist; i++) {
