fe43f85f9ca38947ef2a458185880e895957188b
Logan Fu <lfu40@stuy.edu>
2024-01-21 16:26:00 -0500

    finished a basic battle view

--- pokemon/type/urls.py
+++ pokemon/type/urls.py
@@ -7,5 +7,5 @@ app_name = "type"
 urlpatterns = [
     path("", views.TypeListView.as_view(), name="type_list"),
     path("getInfo/<int:pk>", views.TypeDetailView.as_view(), name="type_detail"),
-
+    path("battle/<int:first_id>/<int:second_id>", views.battlePokemon, name="pokemon_battle")
 ]
--- pokemon/type/views.py
+++ pokemon/type/views.py
@@ -28,3 +28,60 @@ class TypeDetailView(DetailView):
 def battlePokemon(request, first_id, second_id):
     first_pokemon = Pokemon.objects.get(id=first_id)
     second_pokemon = Pokemon.objects.get(id=second_id)
+
+    pokemon_context = {}
+    pokemon_context["p1types"] = {}
+    pokemon_context["p2types"] = {}
+
+    for ptype in first_pokemon.type.all():
+        type_name = ptype.name
+        current_multiplier = 1
+        info_string = ""
+        for ptype in second_pokemon.type.all():
+            current_relations = ptype.damage_relations
+            for i in current_relations['double_damage_from']:
+                if type_name == i['name']:
+                    current_multiplier = current_multiplier * 2
+            for i in current_relations['half_damage_from']:
+                if type_name == i['name']:
+                    current_multiplier = current_multiplier * 0.5
+            for i in current_relations['no_damage_from']:
+                if type_name == i['name']:
+                    current_multiplier = current_multiplier * 0
+        
+        if current_multiplier == 0:
+           info_string = type_name.capitalize() + " does no damage against " + second_pokemon.name.capitalize() + "."
+        else:
+            info_string = type_name.capitalize() + " does " + str(current_multiplier) + " times damage against " + second_pokemon.name.capitalize() + "."
+        pokemon_context["p1types"][type_name] = info_string
+    
+    for ptype in second_pokemon.type.all():
+        type_name = ptype.name
+        current_multiplier = 1
+        info_string = ""
+        for ptype in first_pokemon.type.all():
+            current_relations = ptype.damage_relations
+            for i in current_relations['double_damage_from']:
+                if type_name == i['name']:
+                    current_multiplier = current_multiplier * 2
+            for i in current_relations['half_damage_from']:
+                if type_name == i['name']:
+                    current_multiplier = current_multiplier * 0.5
+            for i in current_relations['no_damage_from']:
+                if type_name == i['name']:
+                    current_multiplier = current_multiplier * 0
+        
+        if current_multiplier == 0:
+           info_string = type_name.capitalize() + " does no damage against " + first_pokemon.name.capitalize() + "."
+        else:
+            info_string = type_name.capitalize() + " does " + str(current_multiplier) + " times damage against " + first_pokemon.name.capitalize() + "."
+        pokemon_context["p2types"][type_name] = info_string
+    
+    context = {
+        'type_context': pokemon_context,
+        'p1_name': first_pokemon.name.capitalize(),
+        'p1_image': first_pokemon.image,
+        'p2_name': second_pokemon.name.capitalize(),
+        'p2_image': second_pokemon.image,
+    }
+    return render(request, 'type/battle.html', context)
